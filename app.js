a46b163fb9b5d92cfd4810864182e6752d2c4056
emon1995 <emonhossain995@gmail.com>
2022-09-03 21:24:17 +0600

    Middlware setup

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Setup View Engine
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// middleware array
+const middleware = [
+    morgan("dev"),
+    express.static("public"),
+    express.urlencoded({extended: true}),
+    express.json()
+]
+app.use(middleware);
 
 app.get("/", (req, res) => {
     res.json({

e75052906f20417cd851c51098cf09faddded6c2
emon1995 <emonhossain995@gmail.com>
2022-09-04 20:12:45 +0600

    Login validation adding

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -8,13 +8,14 @@ const {
 
 const router = require("express").Router();
 const signupValidator = require("../validator/auth/signupValidator");
+const loginValidator = require("../validator/auth/loginValidator");
 
 
 router.get("/signup", signupGetController);
 router.post("/signup", signupValidator, signupPostController);
 
 router.get("/login", loginGetController);
-router.post("/login", loginPostController);
+router.post("/login", loginValidator, loginPostController);
 
 router.get("/logout", logoutController);
 
